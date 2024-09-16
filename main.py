90bc91213b954c9afd36c6753773c4002d73fbe0
Lam Hao <lamtranphuchao@gmail.com>
2024-09-16 21:28:00 +0700

    add uploading image query feature

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form, File, UploadFile
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
@@ -11,8 +11,7 @@ import json
 import torch
 from transformers import AutoModel
 from translate import translate_vietnamese_to_english
-import clip
-import open_clip
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -20,94 +19,85 @@ templates = Jinja2Templates(directory="templates")
 # Cung cấp tệp tĩnh từ thư mục "static"
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-IMAGE_FOLDER = "images"
+IMAGE_FOLDER = r"G:\.shortcut-targets-by-id\1StdpWNNHw_g3qHkaedeDXNHgz9GzrJ1L\AIC2024_Hubew\Keyframes_TransNetV2"
+
 
 # Tải các mô hình sẵn có
-model_jina = AutoModel.from_pretrained(
-    'jinaai/jina-clip-v1', trust_remote_code=True)
-model_xlm, _, preprocess_xlm = open_clip.create_model_and_transforms(
-    'xlm-roberta-large-ViT-H-14', pretrained='frozen_laion5b_s13b_b90k')
-model_siglip, _, preprocess_siglip = open_clip.create_model_and_transforms(
-    'ViT-SO400M-14-SigLIP-384', pretrained='webli')
+model_jina = AutoModel.from_pretrained('jinaai/jina-clip-v1', trust_remote_code=True)
 
 # Khởi tạo faiss và id2imgfile tương ứng cho từng mô hình
-faiss_indices = {
-    'jinaai/jina-clip-v1': faiss.read_index("dict/faiss_normal_jina.bin"),
-    'xlm-roberta-large-ViT-H-14': faiss.read_index("dict/faiss_xml_ViT.bin"),
-    'ViT-SO400M-14-SigLIP-384': faiss.read_index("dict/faiss_ViT_SigLIP.bin")
-}
-
-id2imgfiles = {
-    'jinaai/jina-clip-v1': json.load(open('dict/id2imgfile_jina.json')),
-    'xlm-roberta-large-ViT-H-14': json.load(open('dict/id2imgfaiss_xml_ViT.json')),
-    'ViT-SO400M-14-SigLIP-384': json.load(open('dict/id2imgfaiss_ViT_SigLIP.json'))
-}
-
-# Điều chỉnh đường dẫn trong id2imgfiles để chỉ lấy hai thành phần cuối
-for model_name in id2imgfiles:
-    for key in id2imgfiles[model_name]:
-        id2imgfiles[model_name][key] = os.path.join(
-            *id2imgfiles[model_name][key].split('/')[-2:])
+# file bin: https://drive.google.com/file/d/13UEWcvYTtyT_7hdHwX6grSDCycmJg4va/view?usp=drive_link
+jina_faiss_indices = faiss.read_index(r"D:\TransNetV2\jina_index\jina_indices.bin")
+# file json: https://drive.google.com/file/d/1-mniCTAX1DrXwOCdnfsx1YXYnlMo6RJk/view?usp=drive_link
+id2imgfiles = json.load(open(r"D:\TransNetV2\image_paths\image_paths.json"))
 
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "models": list(faiss_indices.keys())})
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+def path2html(indices):
+    retrieved_images = []
+    for i in indices[0]:
+        retrieved_image_path = id2imgfiles[f'{i}']
+        retrieved_images.append({'path': retrieved_image_path, 
+                                 'idx': i})
+
+    img_htmls = []
+    for retrived_image in retrieved_images:
+        full_path = os.path.join(IMAGE_FOLDER, retrived_image['path'])
+        try:
+            image = Image.open(full_path)
+        except:
+            image = Image.new('RGB', (1280, 720), (0, 0, 0))
+        buffered = io.BytesIO()
+        image.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        img_html = f'''
+        <div onclick="enlargeImage(this)">
+            <!-- id: {retrived_image['idx']:06} -->
+            <p>{full_path[-17:]}</p>
+            <img src="data:image/jpeg;base64,{img_str}" alt="Image"/>
+        </div>
+        '''
+        img_htmls.append(img_html)
+    return img_htmls
 
 
 @app.post("/display_images")
-async def display_images(query: str = Form(...), k: int = Form(...), model_names: list[str] = Form(...)):
+async def display_images(query: str = Form(...), k: int = Form(...)):
     query = translate_vietnamese_to_english(query)
 
-    image_count = {}
+    text_embedding = model_jina.encode_text(query)
+    text_embedding = text_embedding.reshape((1, -1))
+    _, indices = jina_faiss_indices.search(text_embedding, k)
 
-    for model_name in model_names:
-        idx = faiss_indices[model_name]
-        id2imgfile = id2imgfiles[model_name]
-        if model_name == 'jinaai/jina-clip-v1':
-            text_embedding = model_jina.encode_text(query)
-        else:
-            model = model_xlm if model_name == 'xlm-roberta-large-ViT-H-14' else model_siglip
-            tokenizer = open_clip.get_tokenizer(model_name)
-            text_tokens = tokenizer([query])
-            with torch.no_grad():
-                text_embedding = model.encode_text(text_tokens)
-
-        text_embedding = text_embedding.reshape((1, -1))
-        _, indices = idx.search(text_embedding, k)
-
-        # Update image count
-        for idx in indices[0]:
-            img_path = id2imgfile[str(idx)]
-            if img_path in image_count:
-                image_count[img_path] += 1
-            else:
-                image_count[img_path] = 1
-
-    # Categorize images by tier
-    tiered_images = {f'Tier {i+1}': [] for i in range(len(faiss_indices))}
-    for img_path, count in image_count.items():
-        tier = len(faiss_indices) - count
-        tiered_images[f'Tier {tier+1}'].append(img_path)
-
-    img_htmls = []
-    for tier, images in tiered_images.items():
-        if images:
-            if len(model_names) > 1:
-                img_htmls.append(f"<h2>{tier}</h2>")
-            for file_name in images:
-                full_path = os.path.join(
-                    IMAGE_FOLDER, file_name.replace('/', os.sep))
-                image = Image.open(full_path)
-                buffered = io.BytesIO()
-                image.save(buffered, format="WEBP")
-                img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
-                img_html = f'''
-                <div>
-                    <p>{file_name}</p>
-                    <img src="data:image/webp;base64,{img_str}" alt="Image" />
-                </div>
-                '''
-                img_htmls.append(img_html)
+    img_htmls = path2html(indices)
 
     return JSONResponse(content={"image_data": "".join(img_htmls)})
+
+
+@app.post("/display_images2")
+async def display_images(image: UploadFile = File(...), k: int = Form(...)):
+    contents = await image.read()
+    image = Image.open(io.BytesIO(contents))  # Open it as an image using PIL
+
+    img_embedding = model_jina.encode_image(image)
+    img_embedding = img_embedding.reshape((1, -1))
+    _, indices = jina_faiss_indices.search(img_embedding, k)
+
+    img_htmls = path2html(indices)
+
+    return JSONResponse(content={"image_data": "".join(img_htmls)})
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    contents = await file.read()
+    buffered = io.BytesIO(contents)  # Open it as an image using PIL
+    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+    img_html = f'''
+    <img src="data:image/jpeg;base64,{img_str}" alt="Image"/>
+    '''
+    return JSONResponse(content={"image_data": img_html})
